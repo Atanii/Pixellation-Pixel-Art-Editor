440d60ed62ba15c8ed4d506757b952d5a99997ba
Jeremi <kdrisaac42@gmail.com>
2021-03-03 18:02:57 +0100

    Changes: - Dithering updated to handle thickness. - Minor version increment.

--- Pixellation/Pixellation/Tools/DitheringTool.cs
+++ Pixellation/Pixellation/Tools/DitheringTool.cs
@@ -4,6 +4,16 @@ using System.Windows.Media.Imaging;
 
 namespace Pixellation.Tools
 {
+    using MColor = System.Windows.Media.Color;
+
+    /// <summary>
+    /// Delegate for test to decide if checkboard pattern part should be drawn or not to the given coordinate.
+    /// </summary>
+    /// <param name="x">X component of coordinate.</param>
+    /// <param name="y">Y component of coordinate.</param>
+    /// <returns>True if drawable, false otherwise.</returns>
+    public delegate bool DitheringTest(int x, int y);
+
     /// <summary>
     /// Tool for "chess-table" pattern drawing.
     /// </summary>
@@ -13,10 +23,30 @@ namespace Pixellation.Tools
 
         public override Cursor ToolCursor { get => _cursor; }
 
-        public override bool ThicknessCompatible => false;
-
         private DitheringTool() : base()
         {
+        }
+
+        /// <summary>
+        /// Test for left click dithering.
+        /// </summary>
+        /// <param name="x">X component of coordinate.</param>
+        /// <param name="y">Y component of coordinate.</param>
+        /// <returns>!(odd X or even Y) -> true, false otherwise</returns>
+        private static bool LeftClickTest(int x, int y)
+        {
+            return !(((x & 1) == 1 || (y & 1) != 1) ^ ((x & 1) != 1 || (y & 1) == 1));
+        }
+
+        /// <summary>
+        /// Test for right click dithering.
+        /// </summary>
+        /// <param name="x">X component of coordinate.</param>
+        /// <param name="y">Y component of coordinate.</param>
+        /// <returns>!(odd + odd ^ even + even) -> true, false otherwise</returns>
+        private static bool RightClickTest(int x, int y)
+        {
+            return !(((x & 1) == 1 && (y & 1) == 1) ^ ((x & 1) != 1 && (y & 1) != 1));
         }
 
         /// <summary>
@@ -29,39 +59,26 @@ namespace Pixellation.Tools
 
             var p = e.GetPosition(_layer).DivideByIntAsIntPoint(_magnification);
 
-            // odd X or even Y -> return
-            if (e.LeftButton == MouseButtonState.Pressed &&
-                (((p.X & 1) == 1 || (p.Y & 1) != 1) ^
-                ((p.X & 1) != 1 || (p.Y & 1) == 1)))
+            if (e.LeftButton == MouseButtonState.Pressed)
             {
-                return;
+                SetPixelWithThickness(_drawSurface, p.X, p.Y, ToolColor, Thickness, LeftClickTest);
             }
-            // odd + odd ^ even + even
-            if (e.LeftButton != MouseButtonState.Pressed &&
-                (((p.X & 1) == 1 && (p.Y & 1) == 1) ^
-                ((p.X & 1) != 1 && (p.Y & 1) != 1)))
+            if (e.LeftButton != MouseButtonState.Pressed)
             {
-                return;
+                SetPixelWithThickness(_drawSurface, p.X, p.Y, ToolColor, Thickness, RightClickTest);
             }
 
-            if (OutOfBounds(p))
-            {
-                return;
-            }
-
-            _drawSurface.SetPixel(
-                p.X,
-                p.Y,
-                ToolColor
-            );
             if (MirrorMode != MirrorModeStates.OFF)
             {
                 p = Mirr(p);
-                _drawSurface.SetPixel(
-                    p.X,
-                    p.Y,
-                    ToolColor
-                );
+                if (e.LeftButton == MouseButtonState.Pressed)
+                {
+                    SetPixelWithThickness(_drawSurface, p.X, p.Y, ToolColor, Thickness, LeftClickTest);
+                }
+                if (e.LeftButton != MouseButtonState.Pressed)
+                {
+                    SetPixelWithThickness(_drawSurface, p.X, p.Y, ToolColor, Thickness, RightClickTest);
+                }
             }
         }
 
@@ -80,6 +97,44 @@ namespace Pixellation.Tools
             if (IsMouseDown(e))
             {
                 Draw(e);
+            }
+        }
+
+        /// <summary>
+        /// Draws the checkboard pattern with applied thickness.
+        /// </summary>
+        /// <param name="bmp">Bitmap to draw on.</param>
+        /// <param name="x0">X component of coordinate.</param>
+        /// <param name="y0">Y component of coordinate.</param>
+        /// <param name="c">Color to draw with.</param>
+        /// <param name="thickness">Thickness to use.</param>
+        public static void SetPixelWithThickness(WriteableBitmap bmp, int x0, int y0, MColor c, ToolThickness thickness, DitheringTest testMethod)
+        {
+            if (x0 >= 0 && y0 >= 0 && x0 < bmp.PixelWidth && y0 < bmp.PixelHeight && testMethod(x0, y0))
+                bmp.SetPixel(x0, y0, c);
+
+            if (thickness > ToolThickness.NORMAL)
+            {
+                if ((x0 - 1) >= 0 && y0 >= 0 && (x0 - 1) < bmp.PixelWidth && y0 < bmp.PixelHeight && testMethod(x0 - 1, y0))
+                    bmp.SetPixel(x0 - 1, y0, c);
+                if ((x0 - 1) >= 0 && (y0 - 1) >= 0 && (x0 - 1) < bmp.PixelWidth && (y0 - 1) < bmp.PixelHeight && testMethod(x0 - 1, y0 - 1))
+                    bmp.SetPixel(x0 - 1, y0 - 1, c);
+                if (x0 >= 0 && (y0 - 1) >= 0 && x0 < bmp.PixelWidth && (y0 - 1) < bmp.PixelHeight && testMethod(x0, y0 - 1))
+                    bmp.SetPixel(x0, y0 - 1, c);
+
+                if (thickness > ToolThickness.MEDIUM)
+                {
+                    if ((x0 + 1) >= 0 && (y0 + 1) >= 0 && (x0 + 1) < bmp.PixelWidth && (y0 + 1) < bmp.PixelHeight && testMethod(x0 + 1, y0 + 1))
+                        bmp.SetPixel(x0 + 1, y0 + 1, c);
+                    if ((x0 + 1) >= 0 && (y0 - 1) >= 0 && (x0 + 1) < bmp.PixelWidth && (y0 - 1) < bmp.PixelHeight && testMethod(x0 + 1, y0 - 1))
+                        bmp.SetPixel(x0 + 1, y0 - 1, c);
+                    if ((x0 - 1) >= 0 && (y0 + 1) >= 0 && (x0 - 1) < bmp.PixelWidth && (y0 + 1) < bmp.PixelHeight && testMethod(x0 - 1, y0 + 1))
+                        bmp.SetPixel(x0 - 1, y0 + 1, c);
+                    if ((x0 + 1) >= 0 && y0 >= 0 && (x0 + 1) < bmp.PixelWidth && y0 < bmp.PixelHeight && testMethod(x0 + 1, y0))
+                        bmp.SetPixel(x0 + 1, y0, c);
+                    if (x0 >= 0 && (y0 + 1) >= 0 && x0 < bmp.PixelWidth && (y0 + 1) < bmp.PixelHeight && testMethod(x0, y0 + 1))
+                        bmp.SetPixel(x0, y0 + 1, c);
+                }
             }
         }
     }
